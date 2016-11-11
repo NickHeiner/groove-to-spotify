62afd1877b84d1df3110bffdcdb199fb5a3adf1b
Nick Heiner <nth23@cornell.edu>
2016-11-11 14:03:25 -0500

    Add a workflow to get an auth token

---
+++ bin/get-spotify-token.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node --harmony-async-await
+
+const asyncWrapper = require('../lib/async-wrapper'),
+    config = require('../lib/config'),
+    getSpotifyToken = require('../lib/get-spotify-token');
+
+asyncWrapper(() => getSpotifyToken(config('in:clientId')));
